c5b75ea181c2b05204ab1a1e0306f2c0aeb2d099
Mecid <102324700+mecit1997@users.noreply.github.com>
2022-05-15 01:09:39 +0300

    Update broker.py

--- Raspberry Pi 0/broker.py
+++ Raspberry Pi 0/broker.py
@@ -3,17 +3,17 @@
 import subprocess
 import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
-    # This will be called once the client connects
+    
     print(f"Connected with result code {rc}")
     # Subscribe here!
-    client.subscribe("esp8266")
+    client.subscribe("esp8266")             #subscribe olunacak topic
 def on_message(client, userdata, msg):
-    print(f"Message received [{msg.topic}]: {msg.payload}")
+    print(f"Message received [{msg.topic}]: {msg.payload}")   #msg.payload esp'den alinan buffer'lanan mesaj icerigi
     if b"Oksijen Olcum" in msg.payload:
-        subprocess.call(['sh','./send_gmail.sh'])
-client = mqtt.Client("mqtt-test") # client ID "mqtt-test"
+        subprocess.call(['sh','./send_gmail.sh']) #mail gonderme uygulamasi
+client = mqtt.Client("mqtt-test") # client ID "mqtt-test" bos birakilirsa otomatik olusturulur
 client.on_connect = on_connect
 client.on_message = on_message
-client.username_pw_set("mecid", "123")
-client.connect('127.0.0.1', 1883)
+client.username_pw_set("***", "***")  #mqtt kullanici ve sifresi burada belirlenecek olan neyse esp tarafinda da aynisi olusturulmalidir
+client.connect('127.0.0.1', 1883)     #local host ve port numarasi
 client.loop_forever()
